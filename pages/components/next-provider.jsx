9e6f4a79a30d63c4701817704606e1ea5f79b140
arefmaghdam <arefmaghdam@gmail.com>
2024-01-07 22:47:42 +0330

    feat: changed folder structure of this files

---
+++ pages/components/next-provider.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Link from "next/link";
+
+const NextProvider = () => {
+  return (
+    <div>
+      <Link href="/components/nextjs/setting-up">setting-up</Link>
+      <Link href="/components/nextjs/app-structure">app-structure</Link>
+      <Link href="/components/nextjs/routing">routing</Link>
+      <Link href="/components/nextjs/static-routing">static-routing</Link>
+      <Link href="/components/nextjs/dynamic-routes/dynamic-route">
+        dynamic-routing
+      </Link>
+      <Link href="/components/nextjs/dynamic-sub-routes/userID">
+        sub-routing
+      </Link>
+      <Link href="/components/nextjs/get-routes-value">get-routes-value</Link>
+      <Link href="/components/nextjs/link-component">link-component</Link>
+      <Link href="/components/nextjs/dynamic-routes-uses">
+        dynamic-routes-uses
+      </Link>
+      <Link href="/components/nextjs/page-displaying-methods">
+        page-displaying-methods
+      </Link>
+      <Link href="/components/nextjs/adding-404-page">adding-404-page</Link>
+      <Link href="/components/nextjs/pre-rendering">pre-rendering</Link>
+      <Link href="/components/nextjs/getStaticProps-method">
+        getStaticProps-method
+      </Link>
+      <Link href="/components/nextjs/server-side-code-running">
+        server-side-code-running
+      </Link>
+      <Link href="/components/nextjs/dynamic-parameters-uses">
+        dynamic-parameters-uses
+      </Link>
+      <Link href="/components/nextjs/getStaticPaths-method">
+        getStaticPaths-method
+      </Link>
+      <Link href="/components/nextjs/fallback-pages">fallback-pages</Link>
+      <Link href="/components/nextjs/dynamic-display-page">
+        dynamic-display-page
+      </Link>
+      <Link href="/components/nextjs/getServerSideProps-method">
+        getServerSideProps-method
+      </Link>
+      <Link href="/components/nextjs/context-parameter-SSR">
+        context-parameter-SSR
+      </Link>
+      <Link href="/components/nextjs/implementing-dynamic-pages">
+        implementing-dynamic-pages
+      </Link>
+    </div>
+  );
+};
+
+export default NextProvider;
