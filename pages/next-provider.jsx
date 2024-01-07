083fe3c30509edf7f22ff7fa0248c402ba480875
arefmaghdam <arefmaghdam@gmail.com>
2024-01-07 11:32:10 +0330

    fix:set orders of link based on pre-requirements of learning

--- pages/next-provider.jsx
+++ pages/next-provider.jsx
@@ -4,37 +4,37 @@ import Link from "next/link";
 const NextProvider = () => {
   return (
     <div>
-      <Link href="/nextjs/adding-404-page">adding-404-page</Link>
+      <Link href="/nextjs/setting-up">setting-up</Link>
       <Link href="/nextjs/app-structure">app-structure</Link>
-      <Link href="/nextjs/context-parameter-SSR">context-parameter-SSR</Link>
-      <Link href="/nextjs/dynamic-display-page">dynamic-display-page</Link>
-      <Link href="/nextjs/dynamic-parameters-uses">
-        dynamic-parameters-uses
-      </Link>
-      <Link href="/nextjs/dynamic-routes-uses">dynamic-routes-uses</Link>
+      <Link href="/nextjs/routing">routing</Link>
+      <Link href="/nextjs/static-routing">static-routing</Link>
       <Link href="/nextjs/dynamic-routing">dynamic-routing</Link>
-      <Link href="/nextjs/fallback-pages">fallback-pages</Link>
+      <Link href="/nextjs/sub-routing">sub-routing</Link>
       <Link href="/nextjs/get-routes-value">get-routes-value</Link>
-      <Link href="/nextjs/getServerSideProps-method">
-        getServerSideProps-method
-      </Link>
-      <Link href="/nextjs/getStaticPaths-method">getStaticPaths-method</Link>
-      <Link href="/nextjs/getStaticProps-method">getStaticProps-method</Link>
-      <Link href="/nextjs/implementing-dynamic-pages">
-        implementing-dynamic-pages
-      </Link>
       <Link href="/nextjs/link-component">link-component</Link>
+      <Link href="/nextjs/dynamic-routes-uses">dynamic-routes-uses</Link>
       <Link href="/nextjs/page-displaying-methods">
         page-displaying-methods
       </Link>
+      <Link href="/nextjs/adding-404-page">adding-404-page</Link>
       <Link href="/nextjs/pre-rendering">pre-rendering</Link>
-      <Link href="/nextjs/routing">routing</Link>
+      <Link href="/nextjs/getStaticProps-method">getStaticProps-method</Link>
       <Link href="/nextjs/server-side-code-running">
         server-side-code-running
       </Link>
-      <Link href="/nextjs/setting-up">setting-up</Link>
-      <Link href="/nextjs/static-routing">static-routing</Link>
-      <Link href="/nextjs/sub-routing">sub-routing</Link>
+      <Link href="/nextjs/dynamic-parameters-uses">
+        dynamic-parameters-uses
+      </Link>
+      <Link href="/nextjs/getStaticPaths-method">getStaticPaths-method</Link>
+      <Link href="/nextjs/fallback-pages">fallback-pages</Link>
+      <Link href="/nextjs/dynamic-display-page">dynamic-display-page</Link>
+      <Link href="/nextjs/getServerSideProps-method">
+        getServerSideProps-method
+      </Link>
+      <Link href="/nextjs/context-parameter-SSR">context-parameter-SSR</Link>
+      <Link href="/nextjs/implementing-dynamic-pages">
+        implementing-dynamic-pages
+      </Link>
     </div>
   );
 };
