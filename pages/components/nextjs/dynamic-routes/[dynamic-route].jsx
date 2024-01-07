9ecdf77933a911693b2cc3140026586f30ce6731
arefmaghdam <arefmaghdam@gmail.com>
2024-01-07 22:21:36 +0330

    feat: review of dynamic routes and implemented a dynamic route

---
+++ pages/components/nextjs/dynamic-routes/[dynamic-route].jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "../../../../styles/Home.module.css";
+import CodePreview from "../../code-preview";
+import Link from "next/link";
+
+const StaticRouting = () => {
+  return (
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL="../../../images/dynamic-routes.png"
+        content={`
+    // this page codes:
+    
+       import React from "react";
+       import styles from "../../../../styles/Home.module.css";
+       import CodePreview from "../../code-preview";
+       import Link from "next/link";
+
+       const StaticRouting = () => {
+         return (
+           <div className={styles.subPages}>
+             <Link href="/">back</Link>
+           </div>
+         );
+       };
+       
+       export default StaticRouting;
+
+    // there is a standard for this task:
+    // we use [] and write file name into []
+    // for example:
+    // pages/[userID].jsx => /user1 or /user2 or ... this means [userID] can be every thing 
+    // you can see in below:
+      `}
+      />
+      ;
+    </div>
+  );
+};
+
+export default StaticRouting;
