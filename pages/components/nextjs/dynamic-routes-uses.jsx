a7867d0cc8efd0e019da4243bbc1737fc0cc36cf
arefmaghdam <arefmaghdam@gmail.com>
2024-01-07 22:51:10 +0330

    fix: changed address in folder structure for all files of next folder

---
+++ pages/components/nextjs/dynamic-routes-uses.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../../components/code-preview";
+import Link from "next/link";
+
+const DynamicRouting = () => {
+  return (
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL="../../../images/routing.png"
+        content={`
+    // next.js 12 routes are based on folder structure and pages folder
+    // pages/_app.jsx => identifying the active page
+    // pages/index.jsx => "/"
+    // pages/about.jsx => "/about" => about is a static value
+    // pages/components/index.jsx => "/components/" => components is a static value
+    // pages/components/products.jsx => "/components/products/" => products is a static value
+    // all of the above routes that we were used, are static routes same as below picture:
+      `}
+      />
+    </div>
+  );
+};
+
+export default DynamicRouting;
