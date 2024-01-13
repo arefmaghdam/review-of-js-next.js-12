c0685b9b951181b96e95698a3a7ace0f59605ec8
arefmaghdam <arefmaghdam@gmail.com>
2024-01-13 12:43:26 +0330

    feat: review of pre-rendering and its concept

--- pages/components/nextjs/pre-rendering.jsx
+++ pages/components/nextjs/pre-rendering.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../code-preview";
+import Link from "next/link";
 
 const PreRendering = () => {
   return (
-    <div>
-      PreRendering
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        content={`
+    // nextjs is better than react for seo challenge resolving reason
+    // and there is html tags in source page of nextjs apps
+    // so search engines can detect nextjs pages and seo problem is solving
+    // this task do using prerendering or pre generating
+    // this means nextjs apps is rendering in server and result is sending for client
+    // nextjs apps are SSR defaultly, note that all of the nextjs pages are SSR defaultly.
+      `}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default PreRendering
+export default PreRendering;
