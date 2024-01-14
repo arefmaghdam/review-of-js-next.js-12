118a5bfbc0bc31334311ad2a246c1d725a899756
arefmaghdam <arefmaghdam@gmail.com>
2024-01-14 19:17:40 +0330

    feat: review of context parameter in getServerSideProps method and implementation

--- pages/components/nextjs/context-parameter-SSR.jsx
+++ pages/components/nextjs/context-parameter-SSR.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../code-preview";
+import Link from "next/link";
 
-const ContextParameterSSR = () => {
+const ContextParameterSSR = ({ username }) => {
   return (
-    <div>
-      ContextParameterSSR
-    </div>
-  )
-}
+    <div className={styles.subPages}>
+      <h2>{username}</h2>
+      <Link href="/">back</Link>
+      <CodePreview
+        content={`
+    // this page codes:
 
-export default ContextParameterSSR
+    import React from "react";
+    import styles from "../../../styles/Home.module.css";
+    import CodePreview from "../code-preview";
+    import Link from "next/link";
+
+    const ContextParameterSSR = ({ username }) => {
+      return (
+        <div className={styles.subPages}>
+          <h2>{username}</h2>
+          <Link href="/">back</Link>
+        </div>
+      );
+    };
+
+    export const getServerSideProps = async (context) => {
+      const { params, req, res } = context;
+      console.log(req);
+      console.log(res);
+      return {
+        props: {
+          username: "Aref",
+        },
+      };
+    };
+
+    export default ContextParameterSSR;
+
+
+    // we access to user request object and details using context, for example params, req and res
+    // const { params, req, res } = context;
+    // note that user request is sending while refresh is doing
+      `}
+      />
+    </div>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const { params, req, res } = context;
+  console.log(req);
+  console.log(res);
+  return {
+    props: {
+      username: "Aref",
+    },
+  };
+};
+
+export default ContextParameterSSR;
