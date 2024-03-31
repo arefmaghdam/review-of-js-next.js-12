665f7fea4cbfb24847b823db96158e5d75f5eb28
arefmaghdam <arefmaghdam@gmail.com>
2024-03-31 11:06:53 +0330

    feat: review of javascript setting up and development requirements

--- pages/components/javascript/setting-up.jsx
+++ pages/components/javascript/setting-up.jsx
@@ -10,7 +10,10 @@ const SettingUp = () => {
       <CodePreview
         imgURL=""
         content={`
-    // 
+    // we use VSC for javascript development environment, also we need to a browser that translate my codes, we use chrome and chrome v8 engine for this course
+    // we use javascript in desktop and mobile and backend apps that we use node.js for translate or interpret our codes instead of chrome v8 engine
+    // we need some extentions in vsc that are:
+    // 1 auto rename tag 2 ES7 react/redux/graphql/ react-native snippets3 live server 4 material icon theme 5 prettier- code formatter
       `}
       />
     </div>
