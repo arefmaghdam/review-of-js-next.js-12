2f67a07aec64e7a724cd89681fbbd8be72dc2884
arefmaghdam <arefmaghdam@gmail.com>
2024-03-31 11:33:47 +0330

    feat: review of javascript getting output

--- pages/components/javascript/get-output.jsx
+++ pages/components/javascript/get-output.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../../components/code-preview";
+import Link from "next/link";
 
 const GetOutput = () => {
   return (
-    <div>
-      GetOutput
-    </div>
-  )
-}
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL=""
+        content={`
+    // we create a index.html file:
 
-export default GetOutput
+      <html>
+        <head>
+          <title>JS App</title>
+        </head>
+        <body></body>
+      </html>
+
+    // we have 2 way to add javascript to html file:
+    // 1 using script tag into body tag:
+
+      <html>
+        <head>
+          <title>JS App</title>
+        </head>
+        <body>
+          <script>
+            console.log("Hello World!")
+          </script>
+        </body>
+      </html>
+    
+      // but this is not correct and logical way to add javascript codes into html file, because when our app is very big and complex we face with more problems
+      // 2 using from external files, we create a app.js file for javascript codes and we must connect or link app.js and index.html:
+
+        <html>
+          <head>
+            <title>JS App</title>
+          </head>
+          <body>
+            <script src="app.js"></script>
+          </body>
+        </html>
+
+      // app.js codes:
+
+        console.log("Hello World!")
+      `}
+      />
+    </div>
+  );
+};
+
+export default GetOutput;
