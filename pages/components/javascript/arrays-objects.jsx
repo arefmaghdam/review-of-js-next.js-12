226a523e9404974154f35fc2560972c2acfb4cc4
arefmaghdam <arefmaghdam@gmail.com>
2024-04-01 13:21:19 +0330

    feat: review of javascript arrays and objects fusion

--- pages/components/javascript/arrays-objects.jsx
+++ pages/components/javascript/arrays-objects.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../../components/code-preview";
+import Link from "next/link";
 
 const ArraysObjects = () => {
   return (
-    <div>
-      ArraysObjects
-    </div>
-  )
-}
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL=""
+        content={`
+    //  objects and arrays:
 
-export default ArraysObjects
+      const products: [
+        {
+          id: 1,
+          title: "Book 1",
+          price: 99,
+          exists: true.
+        },
+        {
+          id: 2,
+          title: "Book 2",
+          price: 199,
+          exists: false.
+        },
+        {
+          id: 3,
+          title: "Book 3",
+          price: 299,
+          exists: true,
+        },
+      ]
+
+      console.log(products)
+      `}
+      />
+    </div>
+  );
+};
+
+export default ArraysObjects;
