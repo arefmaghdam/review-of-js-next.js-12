e651b6eed373518e546fc3399ead7945da0cce00
arefmaghdam <arefmaghdam@gmail.com>
2024-04-01 17:37:08 +0330

    feat: review of javascript classes

--- pages/components/javascript/classes.jsx
+++ pages/components/javascript/classes.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../../components/code-preview";
+import Link from "next/link";
 
 const Classes = () => {
   return (
-    <div>
-      Classes
-    </div>
-  )
-}
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL=""
+        content={`
+    // classes:
 
-export default Classes
+      class Product {
+        constructor(title, price) {
+          this.title = title
+          this.price = price
+        }
+
+        productInfo() {
+          return 'Title: {this.title} - Price: {this.price}'
+        }
+      }
+
+      const product1 = new Product("Book 1", 99)
+      const product2 = new Product("Book 2", 199)
+
+      console.log(product1)
+      console.log(product2)
+      console.log(product1.productInfo)
+      console.log(product2.productInfo)
+      `}
+      />
+    </div>
+  );
+};
+
+export default Classes;
