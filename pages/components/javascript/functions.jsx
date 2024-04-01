b825cfd22202d74b2b7bc05372b14ab34e854b10
arefmaghdam <arefmaghdam@gmail.com>
2024-04-01 16:45:15 +0330

    feat: review of javascript functions

--- pages/components/javascript/functions.jsx
+++ pages/components/javascript/functions.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../../components/code-preview";
+import Link from "next/link";
 
 const Functions = () => {
   return (
-    <div>
-      Functions
-    </div>
-  )
-}
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL=""
+        content={`
+    // functions:
 
-export default Functions
+      function addProduct(title, price) {
+        console.log('Title: {title} - Price: {price}')
+        return ('Title: {title} - Price: {price}')
+      }
+
+      const result = addProduct("book", 99)
+      console.log(result)
+
+    // default parameters (modern javascript):
+
+      function addProduct(title = "Book 2", price = 109) {
+        return ('Title: {title} - Price: {price}')
+      }
+
+      const result = addProduct()
+      console.log(result)
+
+    // arrow functions (modern javascript):
+
+      const addProduct = (title, price) => {
+        return ('Title: {title} - Price: {price}')
+      }
+
+      const result = addProduct("Book", 99)
+      console.log(result)
+
+    // when we return just 1 line code we can delete return keyword and {} (modern javascript):
+
+      const addProduct = (title, price) => ('Title: {title} - Price: {price}')
+      `}
+      />
+    </div>
+  );
+};
+
+export default Functions;
