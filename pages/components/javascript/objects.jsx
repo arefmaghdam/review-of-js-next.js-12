4af937f2629c37675ae5d8345f8661d54300e559
arefmaghdam <arefmaghdam@gmail.com>
2024-04-01 13:15:48 +0330

    feat: review of javascript objects

--- pages/components/javascript/objects.jsx
+++ pages/components/javascript/objects.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../../components/code-preview";
+import Link from "next/link";
 
 const Objects = () => {
   return (
-    <div>
-      Objects
-    </div>
-  )
-}
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL=""
+        content={`
+    //  objects:
 
-export default Objects
+      const product = {
+        title: "Book",
+        prrice: 99,
+        exists: true
+      }
+
+      console.log(product.title)
+
+    // object destructuring:
+
+      const product = {
+        title: "Book",
+        prrice: 99,
+        exists: true
+      }
+
+      const {title} = product
+
+      console.log(title)  // Book
+      `}
+      />
+    </div>
+  );
+};
+
+export default Objects;
