ed055a627f218593df612b9d2b7ff5a4f4f9a5b5
arefmaghdam <arefmaghdam@gmail.com>
2024-04-01 17:32:30 +0330

    feat: review of javascript constructor function and its methods

--- pages/components/javascript/constructor.jsx
+++ pages/components/javascript/constructor.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../../components/code-preview";
+import Link from "next/link";
 
 const Constructor = () => {
   return (
-    <div>
-      Constructor
-    </div>
-  )
-}
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL=""
+        content={`
+    // javascript support OOP paradigm and we want to review constructor, cunstructor function name wrote UpperCase:
+    // functions into constructor is named methods.
 
-export default Constructor
+      function Product(title, price) {
+        this.title = title
+        this.price = price
+        this.productInfo = function() {
+          return 'Title: {this.title} - Price: {this.price}'
+        }
+      }
+
+    // usage:
+
+      const product1 = new Product("Book 1", 99)
+      const product2 = new Product("Book 2", 199)
+
+      console.log(product1)
+      console.log(product2)
+      console.log(product1.productInfo())
+      console.log(product2.productInfo())
+      `}
+      />
+    </div>
+  );
+};
+
+export default Constructor;
