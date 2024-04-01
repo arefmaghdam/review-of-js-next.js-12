47fefaca6b706ddc7a17e30d04a2773466e8e6c6
arefmaghdam <arefmaghdam@gmail.com>
2024-04-01 16:26:07 +0330

    feat: review of javascript conditions

--- pages/components/javascript/conditions.jsx
+++ pages/components/javascript/conditions.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/Home.module.css";
+import CodePreview from "../../components/code-preview";
+import Link from "next/link";
 
 const Conditions = () => {
   return (
-    <div>
-      Conditions
-    </div>
-  )
-}
+    <div className={styles.subPages}>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL=""
+        content={`
+    // conditions
 
-export default Conditions
+      const price = 99
+      const newPrice = 109
+      if (newPrice > price) {
+        console.log("discount added!")
+      } else if (newPrice < price) {
+        console.log("discount doesnt contain")
+      } else {
+        console.log("price is fit")
+      }
+
+    // note that we shouldnt use else and we must use if for better performance
+      `}
+      />
+    </div>
+  );
+};
+
+export default Conditions;
